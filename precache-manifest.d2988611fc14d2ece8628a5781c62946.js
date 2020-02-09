8e9550a399f0c216e788e01e178ba6abe7ddf8f7
Johnny Reina <johnny.r.reina@gmail.com>
2020-02-09 17:59:34 -0600

    Updates

---
+++ precache-manifest.d2988611fc14d2ece8628a5781c62946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3314bc2f14045fe9c268709f11cc490",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa6f06a4103b215fe34",
+    "url": "/static/css/2.d9183e08.chunk.css"
+  },
+  {
+    "revision": "62649bb7b74dd620518b",
+    "url": "/static/css/main.b522f36b.chunk.css"
+  },
+  {
+    "revision": "1aa6f06a4103b215fe34",
+    "url": "/static/js/2.9f4778ae.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/static/js/2.9f4778ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62649bb7b74dd620518b",
+    "url": "/static/js/main.fb62a859.chunk.js"
+  },
+  {
+    "revision": "e80acf70ff62d83d6065",
+    "url": "/static/js/runtime-main.d43951fe.js"
+  }
+]);
